01dc39676a8a192cd090c529d8acbca94d637af5
Jonas-Luetolf <jonas.luetolf@outlook.com>
2022-12-01 21:34:50 +0100

    update day 1

--- 01-12/part1.py
+++ 01-12/part1.py
@@ -1,14 +1,11 @@
 with open("input.txt","r") as f:
-    inp=f.read()
+    inp=f.read().strip()
 
 data=inp.split("\n\n")
 data=[i.split("\n") for i in data]
 max_sum=(0,0)
 
 for index,i in enumerate(data):
-    if "" in i:
-        continue
-
     s=sum([int(j) for j in i])
     if max_sum[0]<s:
         max_sum=(s,(index+1))
--- 01-12/part2.py
+++ 01-12/part2.py
@@ -1,17 +1,13 @@
 with open("input.txt","r") as f:
-    inp=f.read()
+    inp=f.read().strip()
 
 data=inp.split("\n\n")
 data=[i.split("\n") for i in data]
 sums=[]
 
 for index,i in enumerate(data):
-    if "" in i:
-        continue
-
     s=sum([int(j) for j in i])
     sums.append(s)
 
 sums.sort(reverse=True)
-
 print(sum(sums[:3]))
