41e66c8858bdd56394f62c8d3f833a958091b2db
Jonas-Luetolf <jonas.luetolf@outlook.com>
2022-12-03 10:14:16 +0100

    add day 3

---
+++ 03-12/part1.py
@@ -0,0 +1,20 @@
+import helper
+
+def isdublicate(l1,l2):
+    for i in l1:
+        for j in l2:
+            if i==j:
+                return i
+    return None
+
+def adjust(c):
+    if c>=97: return c-96
+    else: return c-38
+
+data=helper.read_inp("input.txt")
+dublicates=[]
+for i in data:
+    dublicates.append(isdublicate(i[:int(len(i)/2)],i[int(len(i)/2):]))
+
+dublicates=map(adjust,list(map(ord,dublicates)))
+print(sum(dublicates))
---
+++ 03-12/part2.py
@@ -0,0 +1,22 @@
+import helper
+
+def isdublicate(l1,l2,l3):
+    for i in l1:
+        for j in l2:
+            for k in l3:
+                if i==j==k:
+                    return i
+    return None
+
+def adjust(c):
+    if c>=97: return c-96
+    else: return c-38
+
+data=helper.read_inp("input.txt")
+dublicates=[]
+
+for i in range(2,len(data),3):
+    dublicates.append(isdublicate(data[i-2],data[i-1],data[i]))
+
+dublicates=map(adjust,list(map(ord,dublicates)))
+print(sum(dublicates))
