c592815938443f9a02db0c9491c40e14b17d7595
Jonas-Luetolf <jonas.luetolf@outlook.com>
2022-12-14 00:43:56 +0100

    add day 09

--- 09-12/part1.py
+++ 09-12/part1.py
@@ -2,9 +2,10 @@ import helper
 
 data=helper.read_inp("input.txt")
 
-T=[1000,1000]
-H=[1000,1000]
-pos=["10001000"]
+
+T=[0,0]
+H=[0,0]
+pos=[str([0,0])]
 
 
 for step in data:
@@ -15,11 +16,19 @@ for step in data:
         elif d=="R": H[1]+=1
         elif d=="L": H[1]-=1
 
-        if d == "U" and (H[0]+1)<(T[0]): T=[H[0]+1,H[1]]
-        elif d== "D" and (H[0]-1)>(T[0]): T=[H[0]-1,H[1]]
-        elif d == "R" and (H[1]-1)>(T[1]): T=[H[0],H[1]-1]
-        elif d == "L" and (H[1]+1)<(T[1]): T=[H[0],H[1]+1]
-        pos.append("".join(list(map(str,T))))
+        if d == "U" and (H[0]+1)<(T[0]):
+            T=[H[0]+1,H[1]]
+
+        elif d== "D" and (H[0]-1)>(T[0]):
+            T=[H[0]-1,H[1]]
+
+        elif d == "R" and (H[1]-1)>(T[1]):
+            T=[H[0],H[1]-1]
+
+        elif d == "L" and (H[1]+1)<(T[1]):
+            T=[H[0],H[1]+1]
+        
+        pos.append(str(T))
 
 
 print(len(set(pos)))
